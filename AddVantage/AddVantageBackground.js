643ee27a7117840887e9d86c9c8c0f26518158b9
Mrlilal <37249021+Mrlilal@users.noreply.github.com>
2020-04-19 21:57:04 -0400

    Updated to version 1.2.2
    Added and changed some comments. Created all of the skill functions. Changed the color of a console log.

--- AddVantage/AddVantageBackground.js
+++ AddVantage/AddVantageBackground.js
@@ -1,4 +1,4 @@
-// Version 1.2.1
+// Version 1.2.2
 // Start of the injenction function
 var attemptInjection = (rollType, rollName, setting) => {
     // Check if there are already been icons injected
@@ -20,24 +20,26 @@ var attemptInjection = (rollType, rollName, setting) => {
         var add_img_style = document.createAttribute('STYLE');
         // Create an attribute for the ID of the image.
         var add_img_id = document.createAttribute('ID');
-        // The ID that is added (to the 'ID' attribute).
+        // Add a value to the now-created ID.
         add_img_id.value = `AddVantage_${rollType}`;
 
         // Start checking for the settings
         if (setting == 'advantage') {
-            // The image that is added (to the 'SRC' attribute).
+            // Add the value of the image source.
             add_img_src.value = imageAdvantage;
-            // The styling that is added (to the 'STYLE' attribute).
+            // Add the value of the styling of the image.
             add_img_style.value = imageAdvantageStyling;
             // Add the image source ('SRC') to the image tag ('IMG').
             add_img_tag.setAttributeNode(add_img_src);
             // Add the image styling ('STYLE') to the image tag ('IMG').
             add_img_tag.setAttributeNode(add_img_style);
-            // Add the image styling ('STYLE') to the image tag ('IMG').
+            // Add the image ID ('ID') to the image tag ('IMG').
             add_img_tag.setAttributeNode(add_img_id);
 
+            // Create the whole image tag for the button.
             document.getElementsByName(rollType)[0].appendChild(add_img_tag);
 
+            // Log that the icon was added.
             console.log(`%cAddVantage: The advantage icon for the ${rollName} was successfully added.`, 'color: #32CD32;');
         } else if (setting == 'disadvantage') {
             // The image that is added (to the 'SRC' attribute).
@@ -48,22 +50,26 @@ var attemptInjection = (rollType, rollName, setting) => {
             add_img_tag.setAttributeNode(add_img_src);
             // Add the image styling ('STYLE') to the image tag ('IMG').
             add_img_tag.setAttributeNode(add_img_style);
-            // Add the image styling ('STYLE') to the image tag ('IMG').
+            // Add the image ID ('ID') to the image tag ('IMG').
             add_img_tag.setAttributeNode(add_img_id);
 
+            // Create the whole image tag for the button.
             document.getElementsByName(rollType)[0].appendChild(add_img_tag);
 
-            // Log in the console that the advantage icon for Intelligence was added.
+            // Log in the console that the disadvantage icon for Intelligence was added.
             console.log(`%cAddVantage: The disadvantage icon for the ${rollName} was successfully added.`, 'color: #32CD32;');
         } else if (setting == 'none') {
+            // Log in the console that the setting is off for adding an image.
             console.log(`%cAddVantage: Setting for ${rollName} is "none." Continuing to the next setting.`, 'color: #32CD32;');
         } else {
+            // Log that some settings are not set up properly.
             console.log('%c---------------------------------------------', 'color: red;');
             console.log(`%cAddVantage: Setting for ${rollName} is not set properly. Please ensure that all settings are configured properly.`, 'color: red;');
             console.log('%c---------------------------------------------', 'color: red;');
         };
     } else {
-        console.log(`%cAddVantage: The icons are already injected. Skipping attempted injection.`, 'color: yellow;');
+        // Log that the icons already exist.
+        console.log(`%cAddVantage: The icons are already injected. Skipping attempted injection.`, 'color: #32CD32;');
     };
 }
 
@@ -73,6 +79,7 @@ function addVantageSaveStrength() {
     let rollName = 'Strength Saving Throw';
     let setting = savingThrowStrength;
 
+    // Start the injection while passing the variables.
     attemptInjection(rollType, rollName, setting);
 };
 
@@ -82,6 +89,7 @@ function addVantageSaveDexterity() {
     let rollName = 'Dexterity Saving Throw';
     let setting = savingThrowDexterity;
 
+    // Start the injection while passing the variables.
     attemptInjection(rollType, rollName, setting);
 };
 
@@ -91,6 +99,7 @@ function addVantageSaveConstitution() {
     let rollName = 'Constitution Saving Throw';
     let setting = savingThrowConstitution;
 
+    // Start the injection while passing the variables.
     attemptInjection(rollType, rollName, setting);
 };
 
@@ -101,6 +110,7 @@ function addVantageSaveIntelligence() {
     let rollName = 'Intelligence Saving Throw';
     let setting = savingThrowIntelligence;
 
+    // Start the injection while passing the variables.
     attemptInjection(rollType, rollName, setting);
 };
 
@@ -110,6 +120,7 @@ function addVantageSaveWisdom() {
     let rollName = 'Wisdom Saving Throw';
     let setting = savingThrowWisdom;
 
+    // Start the injection while passing the variables.
     attemptInjection(rollType, rollName, setting);
 };
 
@@ -119,12 +130,193 @@ function addVantageSaveCharisma() {
     let rollName = 'Charisma Saving Throw';
     let setting = savingThrowCharisma;
 
+    // Start the injection while passing the variables.
     attemptInjection(rollType, rollName, setting);
 };
 
+// Function for adding the image to the save/skill that you want.
+function addVantageSkillAcrobatics() {
+    let rollType = 'roll_acrobatics';
+    let rollName = 'Acrobatics Skill Check';
+    let setting = skillCheckAcrobatics;
+
+    // Start the injection while passing the variables.
+    attemptInjection(rollType, rollName, setting);
+};
+
+// Function for adding the image to the save/skill that you want.
+function addVantageSkillAnimalHandling() {
+    let rollType = 'roll_animal_handling';
+    let rollName = 'Animal Handling Skill Check';
+    let setting = skillCheckAnimalHandling;
+
+    // Start the injection while passing the variables.
+    attemptInjection(rollType, rollName, setting);
+};
+
+// Function for adding the image to the save/skill that you want.
+function addVantageSkillArcana() {
+    let rollType = 'roll_arcana';
+    let rollName = 'Arcana Skill Check';
+    let setting = skillCheckArcana;
+
+    // Start the injection while passing the variables.
+    attemptInjection(rollType, rollName, setting);
+};
+
+// Function for adding the image to the save/skill that you want.
+function addVantageSkillAthletics() {
+    let rollType = 'roll_athletics';
+    let rollName = 'Athletics Skill Check';
+    let setting = skillCheckAthletics;
+
+    // Start the injection while passing the variables.
+    attemptInjection(rollType, rollName, setting);
+};
+
+// Function for adding the image to the save/skill that you want.
+function addVantageSkillDeception() {
+    let rollType = 'roll_deception';
+    let rollName = 'Deception Skill Check';
+    let setting = skillCheckDeception;
+
+    // Start the injection while passing the variables.
+    attemptInjection(rollType, rollName, setting);
+};
+
+// Function for adding the image to the save/skill that you want.
+function addVantageSkillHistory() {
+    let rollType = 'roll_history';
+    let rollName = 'History Skill Check';
+    let setting = skillCheckHistory;
+
+    // Start the injection while passing the variables.
+    attemptInjection(rollType, rollName, setting);
+};
+
+// Function for adding the image to the save/skill that you want.
+function addVantageSkillInsight() {
+    let rollType = 'roll_insight';
+    let rollName = 'Insight Skill Check';
+    let setting = skillCheckInsight;
+
+    // Start the injection while passing the variables.
+    attemptInjection(rollType, rollName, setting);
+};
+
+// Function for adding the image to the save/skill that you want.
+function addVantageSkillIntimidation() {
+    let rollType = 'roll_intimidation';
+    let rollName = 'Intimidation Skill Check';
+    let setting = skillCheckIntimidation;
+
+    // Start the injection while passing the variables.
+    attemptInjection(rollType, rollName, setting);
+};
+
+// Function for adding the image to the save/skill that you want.
+function addVantageSkillInvestigation() {
+    let rollType = 'roll_investigation';
+    let rollName = 'Investigation Skill Check';
+    let setting = skillCheckInvestigation;
+
+    // Start the injection while passing the variables.
+    attemptInjection(rollType, rollName, setting);
+};
+
+// Function for adding the image to the save/skill that you want.
+function addVantageSkillMedicine() {
+    let rollType = 'roll_medicine';
+    let rollName = 'Medicine Skill Check';
+    let setting = skillCheckMedicine;
+
+    // Start the injection while passing the variables.
+    attemptInjection(rollType, rollName, setting);
+};
+
+// Function for adding the image to the save/skill that you want.
+function addVantageSkillNature() {
+    let rollType = 'roll_nature';
+    let rollName = 'Nature Skill Check';
+    let setting = skillCheckNature;
+
+    // Start the injection while passing the variables.
+    attemptInjection(rollType, rollName, setting);
+};
+
+// Function for adding the image to the save/skill that you want.
+function addVantageSkillPerception() {
+    let rollType = 'roll_perception';
+    let rollName = 'Perception Skill Check';
+    let setting = skillCheckPerception;
+
+    // Start the injection while passing the variables.
+    attemptInjection(rollType, rollName, setting);
+};
+
+// Function for adding the image to the save/skill that you want.
+function addVantageSkillPerformance() {
+    let rollType = 'roll_performance';
+    let rollName = 'Performance Skill Check';
+    let setting = skillCheckPerformance;
+
+    // Start the injection while passing the variables.
+    attemptInjection(rollType, rollName, setting);
+};
+
+// Function for adding the image to the save/skill that you want.
+function addVantageSkillPersuasion() {
+    let rollType = 'roll_persuasion';
+    let rollName = 'Persuasion Skill Check';
+    let setting = skillCheckPersuasion;
+
+    // Start the injection while passing the variables.
+    attemptInjection(rollType, rollName, setting);
+};
+
+// Function for adding the image to the save/skill that you want.
+function addVantageSkillReligion() {
+    let rollType = 'roll_religion';
+    let rollName = 'Religion Skill Check';
+    let setting = skillCheckReligion;
+
+    // Start the injection while passing the variables.
+    attemptInjection(rollType, rollName, setting);
+};
+
+// Function for adding the image to the save/skill that you want.
+function addVantageSkillSleightOfHand() {
+    let rollType = 'roll_sleight_of_hand';
+    let rollName = 'Sleight of Hand Skill Check';
+    let setting = skillCheckSleightOfHand;
+
+    // Start the injection while passing the variables.
+    attemptInjection(rollType, rollName, setting);
+};
+
+// Function for adding the image to the save/skill that you want.
+function addVantageSkillStealth() {
+    let rollType = 'roll_stealth';
+    let rollName = 'Stealth Skill Check';
+    let setting = skillCheckStealth;
+
+    // Start the injection while passing the variables.
+    attemptInjection(rollType, rollName, setting);
+};
+
+// Function for adding the image to the save/skill that you want.
+function addVantageSkillSurvival() {
+    let rollType = 'roll_survival';
+    let rollName = 'Survival Skill Check';
+    let setting = skillCheckSurvival;
+
+    // Start the injection while passing the variables.
+    attemptInjection(rollType, rollName, setting);
+};
 
 // Start all functions.
 function startLoading() {
+    // Log that the code actually loaded.
     console.log('%cAddVantage: Code initialized, attempting injection.', 'color: #32CD32;');
 
     addVantageSaveStrength();
@@ -133,4 +325,23 @@ function startLoading() {
     addVantageSaveIntelligence();
     addVantageSaveWisdom();
     addVantageSaveCharisma();
+
+    addVantageSkillAcrobatics();
+    addVantageSkillAnimalHandling();
+    addVantageSkillArcana();
+    addVantageSkillAthletics();
+    addVantageSkillDeception();
+    addVantageSkillHistory();
+    addVantageSkillInsight();
+    addVantageSkillIntimidation();
+    addVantageSkillInvestigation();
+    addVantageSkillMedicine();
+    addVantageSkillNature();
+    addVantageSkillPerception();
+    addVantageSkillPerformance();
+    addVantageSkillPersuasion();
+    addVantageSkillReligion();
+    addVantageSkillSleightOfHand();
+    addVantageSkillStealth();
+    addVantageSkillSurvival();
 };
