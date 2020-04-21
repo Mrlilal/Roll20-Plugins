e799aef27291cc968b89cb12d4303c3cd3acc73b
Mrlilal <>
2020-04-21 12:50:08 -0400

    Updated the main userscript header.

---
+++ Roll20 Dark Main/Roll20DarkMain.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         AddVantage
+// @namespace    http://github.com/Mrlilal
+// @description  A dark theme for Roll20's main site, based off RedReign's "Roll20 Dark".
+// @author       Mrlilal
+// @version      1.0.0
+// @license      GPL-3.0-or-later
+// @updateURL    https://github.com/Mrlilal/Roll20-Plugins/raw/master/Roll20%20Dark%20Main/Roll20DarkMain.user.js
+// @downloadURL  https://github.com/Mrlilal/Roll20-Plugins/raw/master/Roll20%20Dark%20Main/Roll20DarkMain.user.js
+// @supportURL   https://github.com/Mrlilal/Roll20-Plugins/Roll20%20Dark%20Main/
+
+// @match        https://app.roll20.net/*
+
+// @run-at       document-idle
+// ==/UserScript==
