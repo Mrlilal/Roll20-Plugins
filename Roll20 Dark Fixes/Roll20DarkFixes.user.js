d9857d5eaf2d45038563efed18146eaa9af92d9c
Mrlilal <>
2020-04-20 20:39:06 -0400

    Updated URLs from dev branch to master branch.

--- Roll20 Dark Fixes/Roll20DarkFixes.user.js
+++ Roll20 Dark Fixes/Roll20DarkFixes.user.js
@@ -5,10 +5,10 @@
 // @author       Mrlilal & Colenk
 // @version      1.0.0
 // @license      GPL-3.0-or-later
-// @updateURL    https://github.com/Mrlilal/Roll20-Plugins/raw/roll20-dark-fixes-dev/Roll20%20Dark%20Fixes/Roll20DarkFixes.user.js
-// @downloadURL  https://github.com/Mrlilal/Roll20-Plugins/raw/roll20-dark-fixes-dev/Roll20%20Dark%20Fixes/Roll20DarkFixes.user.js
+// @updateURL    https://github.com/Mrlilal/Roll20-Plugins/raw/master/Roll20%20Dark%20Fixes/Roll20DarkFixes.user.js
+// @downloadURL  https://github.com/Mrlilal/Roll20-Plugins/raw/master/Roll20%20Dark%20Fixes/Roll20DarkFixes.user.js
 // @supportURL   https://github.com/Mrlilal/Roll20-Plugins/AddVantage/
-// @require      https://raw.githubusercontent.com/Mrlilal/Roll20-Plugins/roll20-dark-fixes-dev/Roll20%20Dark%20Fixes/Roll20DarkFixesBackground.js
+// @require      https://raw.githubusercontent.com/Mrlilal/Roll20-Plugins/master/Roll20%20Dark%20Fixes/Roll20DarkFixesBackground.js
 
 // @match        https://app.roll20.net/editor*
 
